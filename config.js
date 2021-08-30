f0cd181cff052c9363782ed6d5b5d64c006a806c
meomuop95 <89782849+meomuop95@users.noreply.github.com>
2021-08-30 19:25:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,15 +6,11 @@ const CONFIG = {
     Meo meo...rù rù...
     Rù rù... meo meo ...`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: 'Meo đang làm gì đó 😙',
+    desc: 'Khum trả lời nhây nha ',
+    btnYes: 'Đang đợi tnhắn á',
+    btnNo: 'Hỏi làm gì. Bơ bây giờ',
+    question:'Dịch thử đoạn đầu coi',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/duythong1412/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+  
 }
